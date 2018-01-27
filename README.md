fa4a48d73e6f3568c29562350d878a4838a20f9c
chenhaoxiang <chenhaoxiang0117@qq.com>
2018-01-28 00:36:17 +0800

    该项目弄完了，进行了一下总结

--- README.md
+++ README.md
@@ -15,3 +15,82 @@ https://github.com/Pay-Group/best-pay-sdk/blob/master/doc/borrowAccount.md
 链接如下  
 https://github.com/Wechat-Group/weixin-java-tools 这个非常全，你看了就知道了  
 https://github.com/Pay-Group/best-pay-sdk 这个就是廖师兄开发的SDK，支付使用的就是该SDK  
+
+
+# linux服务器启动jar包项目
+
+centos7提倡的用法  
+
+cd /ets/systemd/system  
+到这个目录下，新建一个 AAA.service，可以把AAA设置为项目名的  
+
+vim AAA.service  
+文件内容Start:  
+```
+[Unit]  
+Description=AAA #描述  
+After=syslog.target network.target  #依赖  
+
+[Service]  
+Type=simple  
+
+ExecStart=/usr/bin/java -jar /opt/javaapps/AAA.jar  
+#前面是java命令的绝对路径  后面是jar包的绝对路径  
+ExecStop=/bin/kill -15 $MAINPID   
+
+User=root  
+Group=root   
+
+[Install]  
+WantedBy=multi-user.target  
+```
+文件结束END  
+
+使用  
+systemctl start AAA或者  
+systemctl start AAA.service  
+如果被改变了:  
+先运行systemctl daemon-reload再运行systemctl start sell.service  
+
+停止服务:  
+systemctl stop AAA或者  
+systemctl stop AAA.service  
+
+开机自启动:   
+systemctl enable AAA或者   
+systemctl enable AAA.service   
+
+不想开机启动:  
+systemctl disable AAA或者  
+systemctl  disable AAA.service  
+
+
+# 项目总结   
+ 
+## 微信特性  
+模板消息，授权，支付和退款    
+
+## Token认证   
+在卖家端登录管理系统用到  
+我在aop中已经屏蔽了，因为我没有微信开放平台的认证账号，无法登录    
+可以自行去cn.chenhaoxiang.aspect.SellerAuthorizeAspect将类上的注解放开    
+
+## WebSocket消息  
+在买家下订单后，对买家端有消息提示并播放音乐   
+
+## Redis缓存+分布式锁  
+Redis的缓存的话，注意增删改更新缓存，否则会出现无法预知的后果  
+在这里，如果有商品的抢购活动，就可以使用到Redis的分布式锁了  
+
+
+
+我觉得该项目还有一些需要完善的地方  
+比如卖家端没有权限控制  
+比如应用没有独立，项目里面的商品，订单   
+比如哪天修改了商品的代码，会影响到订单的部分  
+应该把商品和订单拆分开来，作为两个独立的应用  
+
+在这个项目中学到了很多。  
+学到的最重要的不是一些知识点的学习，而是项目架构方面的学习，比如DTO，比如工具类，比如From,前端表单数据提交的实体类，比如应用独立，前后端分离，分布式和集群等等。  
+
+在次感谢廖师兄分享的视频教程。  
---
+++ wechatorder.sql
@@ -0,0 +1,99 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : uifuture
+Source Server Version : 50714
+Source Host           : localhost:3306
+Source Database       : wechatorder
+
+Target Server Type    : MYSQL
+Target Server Version : 50714
+File Encoding         : 65001
+
+Date: 2018-01-28 00:29:34
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for order_detail
+-- ----------------------------
+DROP TABLE IF EXISTS `order_detail`;
+CREATE TABLE `order_detail` (
+  `detail_id` varchar(32) NOT NULL,
+  `order_id` varchar(32) NOT NULL COMMENT '订单id',
+  `product_id` varchar(32) NOT NULL COMMENT '商品id',
+  `product_name` varchar(64) NOT NULL COMMENT '商品名字',
+  `product_price` decimal(8,2) NOT NULL COMMENT '商品价格',
+  `product_quantity` int(11) NOT NULL COMMENT '商品数量',
+  `product_icon` varchar(512) NOT NULL COMMENT '商品小图',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`detail_id`),
+  KEY `idx_order_id` (`order_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单详情表';
+
+-- ----------------------------
+-- Table structure for order_master
+-- ----------------------------
+DROP TABLE IF EXISTS `order_master`;
+CREATE TABLE `order_master` (
+  `order_id` varchar(32) NOT NULL,
+  `buyer_name` varchar(32) NOT NULL COMMENT '买家名字',
+  `buyer_phone` varchar(32) NOT NULL COMMENT '买家电话',
+  `buyer_address` varchar(128) NOT NULL COMMENT '买家地址',
+  `buyer_openid` varchar(64) NOT NULL COMMENT '买家微信openid',
+  `order_amount` decimal(8,2) NOT NULL COMMENT '订单总金额',
+  `order_status` tinyint(3) NOT NULL DEFAULT '0' COMMENT '订单状态,默认0，新下单',
+  `pay_status` tinyint(3) NOT NULL DEFAULT '0' COMMENT '支付状态，默认0未支付',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`order_id`),
+  KEY `idx_buyer_openid` (`buyer_openid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='订单表';
+
+-- ----------------------------
+-- Table structure for product_category
+-- ----------------------------
+DROP TABLE IF EXISTS `product_category`;
+CREATE TABLE `product_category` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(64) NOT NULL COMMENT '类目名字',
+  `category_type` int(11) NOT NULL COMMENT '类目编号',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`category_id`),
+  UNIQUE KEY `upe_category_type` (`category_type`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COMMENT='类目表';
+
+-- ----------------------------
+-- Table structure for product_info
+-- ----------------------------
+DROP TABLE IF EXISTS `product_info`;
+CREATE TABLE `product_info` (
+  `product_id` varchar(32) NOT NULL,
+  `product_name` varchar(64) NOT NULL COMMENT '商品名称',
+  `product_price` decimal(8,2) NOT NULL COMMENT '商品单价',
+  `product_stock` int(11) NOT NULL COMMENT '库存',
+  `product_description` varchar(64) DEFAULT NULL COMMENT '描述',
+  `product_icon` varchar(512) DEFAULT NULL COMMENT '小图的url',
+  `category_type` int(11) NOT NULL COMMENT '类目编号',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  `product_status` tinyint(4) NOT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品表';
+
+-- ----------------------------
+-- Table structure for seller_info
+-- ----------------------------
+DROP TABLE IF EXISTS `seller_info`;
+CREATE TABLE `seller_info` (
+  `seller_id` varchar(32) NOT NULL COMMENT '卖家信息表',
+  `username` varchar(32) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `openid` varchar(64) NOT NULL COMMENT '微信openid',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  PRIMARY KEY (`seller_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='卖家信息表';
